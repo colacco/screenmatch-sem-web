5c186e71d02f1d3d070bcf59ce4b02b6b107e6d8
Gabriel <coala@kali.kali>
2025-07-05 13:20:15 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias; // Apenas  lê o json e escreve com o nome do atributo
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty; // processo de serialização e desasserialização com o nome declarado em json.
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias({"Title", "Título"}) String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
